5e19cfbaa52acfa55acbe382dc7ac0684aeddc71
Priyam <priyamgypta123@gmail.com>
2026-09-05 16:28:28 +0530

    Handle API query strings

--- api/_products.js
+++ api/_products.js
@@ -1,13 +1,14 @@
 const { findProduct, listProducts } = require("./catalog");
 
 module.exports = (req, res) => {
-  const identifier = req.query.idOrSlug || req.url.replace(/^\/api\/products\/?/, "");
+  const requestPath = String(req.url || "").split("?", 1)[0];
+  const identifier = req.query?.idOrSlug || requestPath.replace(/^\/api\/products\/?/, "");
 
   if (!identifier) {
     return res.status(200).json(listProducts());
   }
 
-  const product = findProduct(identifier.split("?")[0]);
+  const product = findProduct(identifier);
   if (!product) {
     return res.status(404).json({ error: `No product found with identifier "${identifier}"` });
   }
